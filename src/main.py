2035374afeb149c4ec3f6202ae225e103c3bb794
Foo JH <foojihaw@hotmail.com>
2025-01-11 17:24:06 +0000

    NEW: basic threaded scheduler and API services

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 # core
 from contextlib import asynccontextmanager
 import time
+import threading
 
 # community
 from fastapi import FastAPI
@@ -12,13 +13,27 @@ def my_job():
   print(f"Job completed at: {time.strftime('%Y-%m-%d %H:%M:%S')}")
 
 
+isThreadRunning = True
+
+def speak():
+  print (f'Speech thread started')
+  while isThreadRunning:
+    time.sleep(1)
+
+  print (f'Speech thread stopped')
+
 @asynccontextmanager
 async def lifespan(app: FastAPI):
   scheduler = BackgroundScheduler()
   scheduler.add_job(my_job, 'interval', seconds=10)
   scheduler.start()
   print (f'Schedulder started')
+
+  SpeechThread = threading.Thread(target=speak)
+  SpeechThread.start()
+
   yield
+  isThreadRunning = False
   scheduler.shutdown()
   print (f'FastAPI service stopped')
 
